4c0163ae0c7c180a7a10f08442fd3714ba0f1af8
Kamal <kamalmiftah01@gmail.com>
2024-01-03 02:43:39 +0100

    fixing task 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,18 +1,74 @@
 #include "lists.h"
-listint_t *insert_node(listint_t **head, int number)
+
+/**
+* insert_nodeint_at_index - a function that inserts a new node
+* at a given position.
+* @head: pointer to a pointer to the list.
+* @idx: the index of the list where the new node should be added.
+*	Index starts at 0.
+* @n: the value to be inserted in the new node.
+*
+* Return: he address of the new node, or NULL if it failed.
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newnode;
+		listint_t *newnode;
+	unsigned int i = 0;
+	listint_t *temp;
 
 	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
+
+	if (newnode == NULL || idx > listint_len(*head))
 	{
+		free(newnode);
 		return (NULL);
 	}
 	else
 	{
-		newnode->n = number;
-		newnode->next = *head;
-		*head = newnode;
+		temp = NULL;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (i == 0)
+				{
+					newnode->n = n;
+					newnode->next = *head;
+					*head = newnode;
+					return (newnode);
+				}
+				newnode->next = temp->next;
+				temp->next = newnode;
+				return (newnode);
+			}
+			else if (i + 1 == idx)
+			{
+				temp = *head;
+			}
+			head = &((*head)->next);
+			i++;
+		}
 	}
-	return (newnode);
+	return (NULL);
+}
+
+/**
+* listint_len - a function that calculate the number of nodes in a list.
+* @h: pointer to the list
+*
+* Return: number of nodes.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+		const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
