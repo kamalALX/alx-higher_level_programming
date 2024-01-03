94b9c8cf34fd3c2083fee24e32f2a68cde5942b6
Kamal <kamalmiftah01@gmail.com>
2024-01-03 03:07:28 +0100

    fixing tast 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-idx = 0
+
+i = 0
 for character in range(ord('z'), ord('a') - 1, -1):
-        print("{}".format(chr(character - idx)), end="")
-        idx = 32 if idx == 0 else 0
+    print("{}".format(chr(character - i)), end="")
+    i = 32 if i == 0 else 0
