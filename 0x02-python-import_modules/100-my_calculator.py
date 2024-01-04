cf2cafd4ee958c1e353011e9c94feaf1183a6a1b
Kamal <kamalmiftah01@gmail.com>
2024-01-04 19:36:44 +0100

    fixing task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import calculator_1
+    from calculator_1 import sub, add, mul, div
 
     argc = len(sys.argv) - 1
     if argc != 3:
         print("./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     a, b, operator = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
     if operator not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     elif argc == 3:
         if operator == '+':
-            print("{} {} {} = {}".format(a, operator, b, a + b))
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
         elif operator == '-':
-            print("{} {} {} = {}".format(a, operator, b, a + b))
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
         elif operator == '*':
-            print("{} {} {} = {}".format(a, operator, b, a + b))
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
         elif operator == '/':
-            print("{} {} {} = {}".format(a, operator, b, a + b))
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
