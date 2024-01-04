bd4d9b21e51fd886257488674d1bba1675fc3dea
Kamal <kamalmiftah01@gmail.com>
2024-01-04 08:52:28 +0100

    project: 0x02. Python - import & modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1
+
+    argc = len(sys.argv) - 1
+    if argc != 3:
+        print("./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a, b, operator = int(sys.argv[1]), int(sys.argv[3]), sys.argv[2]
+    if operator not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    elif argc == 3:
+        if operator == '+':
+            print("{} {} {} = {}".format(a, operator, b, a + b))
+        elif operator == '-':
+            print("{} {} {} = {}".format(a, operator, b, a + b))
+        elif operator == '*':
+            print("{} {} {} = {}".format(a, operator, b, a + b))
+        elif operator == '/':
+            print("{} {} {} = {}".format(a, operator, b, a + b))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-import sys
-
-argc = len(sys.argv)
-addition = 0
-
-for i in range(1, argc):
-    addition += int(sys.argv[i])
-print(addition)
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    addition = 0
+    for i in range(1, argc):
+        addition += int(sys.argv[i])
+    print(addition)
