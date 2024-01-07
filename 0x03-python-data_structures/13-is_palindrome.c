5ccbd2fa53d1e79d637f31ffa2d311fb1d15d7b7
Kamal <kamalmiftah01@gmail.com>
2024-01-07 22:08:55 +0100

    solving task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,22 +2,68 @@
 
 /***/
 
-listint_t *reverse(listint_t **head)
+listint_t *reverse(listint_t *head)
 {
-    listint_t *current = *head;
-    listint_t *next = NULL:
-    listint_t *previous = NULL;
+	listint_t *current = head;
+	listint_t *next;
+	listint_t *previous = NULL;
 
-    
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = previous;
-        previous = current;
-        current = next;
-    }
-    return (previous);
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = previous;
+		previous = current;
+		current = next;
+	}
+	return (previous);
 
 }
 
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev_slow = *head;
+	listint_t *midnode = NULL;
+	int res = 1;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		prev_slow = slow;
+		slow = slow->next;
+	}
+
+	if (fast != NULL)
+	{
+		midnode = slow;
+		slow = slow->next;
+	}
+	slow = reverse(slow);
+	fast = *head;
+
+	while (slow != NULL)
+	{
+		if (fast->n != slow->n)
+		{
+			res = 0;
+			break;
+		}
+
+		fast = fast->next;
+		slow = slow->next;
+	}
+
+	slow = reverse(slow);
+
+	if (midnode != NULL)
+	{
+		prev_slow->next = midnode;
+		midnode->next = slow;
+	}
+	else
+		prev_slow->next = slow;
+
+	return res;
+}
 
