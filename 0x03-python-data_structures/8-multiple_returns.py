8ff0194a648f9f2a4e66117da1a7ecf69d27e012
Kamal <kamalmiftah01@gmail.com>
2024-01-07 20:16:26 +0100

    fixing tasks 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     tuple0 = ()
     if len(sentence) == 0:
-        tuple1 = 0, "None"
+        tuple0 = 0, "None"
     else:
         tuple0 = len(sentence), sentence[0]
     return tuple0
