a043e03943869455002f4145861d2c505ea32b46
Kamal <kamalmiftah01@gmail.com>
2024-01-07 19:59:22 +0100

    fixing tasks 8,9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
-
 def multiple_returns(sentence):
-    cr_tuple = (sentence)
-    if (len(sentence) == 0):
-        return 0, None
-    return len(cr_tuple), cr_tuple[0]
+    if (sentence = ""):
+        return (0, None)
+    return (len(sentence), sentence[0])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
     if len(my_list) == 0:
         return None
 
+    max = my_list[0]
     for i in range(len(my_list)):
-        number = my_list[i]
-        if (number > max):
-            max = number
+        if (my_list[i] > max):
+            max = my_list[i]
     return max
