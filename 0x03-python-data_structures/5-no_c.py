9e9dcc838118afa12a35212ec201bd26754af2fe
Kamal <kamalmiftah01@gmail.com>
2024-01-07 19:27:28 +0100

    fixing pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
     string = list(my_string)
-    for idx in range (len(string)):
+    for idx in range(len(string)):
         if string[idx] == 'c' or string[idx] == 'C':
             string[idx] = ''
     return "".join(string)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     max = 0
     if len(my_list) == 0:
         return None
 
-    for i in range (len(my_list)):
+    for i in range(len(my_list)):
         number = my_list[i]
         if (number > max):
             max = number
