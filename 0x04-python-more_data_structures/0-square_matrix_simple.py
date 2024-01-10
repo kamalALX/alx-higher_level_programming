6ee15d310aabf6717f1c028cf6cbe1d25ffed6dd
Kamal <kamalmiftah01@gmail.com>
2024-01-10 15:06:52 +0100

    fixing task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    copy_matrix =[]
+    copy_matrix = []
     for element in matrix:
         res = list(map(lambda x: x * x, element))
         copy_matrix.append(res)
