0fa6b7f4e11600740a1ee20145faecf11b24509a
Kamal <kamalmiftah01@gmail.com>
2024-01-10 15:20:11 +0100

    fixing task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
+        list0 = list(a_dictionary.keys())
         max_int = 0
         max = ""
-            for key, value in a_dictionary.items():
-                if  value > max_int:
-                    max_int = value
+        for key in list0:
+            if a_dictionary[key] > max_int:
+                max_int = a_dictionary[key]
                 max = key
         return max
