b6496c491c87f722725f9de9255896cd06730219
Kamal <kamalmiftah01@gmail.com>
2024-01-22 15:57:20 +0100

    Python - Exceptions : solution of tasks from 0 to 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return None
