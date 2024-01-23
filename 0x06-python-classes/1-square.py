aa328434ac18fea654dbaa4d331e04d8c8c2884e
Kamal <kamalmiftah01@gmail.com>
2024-01-23 18:37:50 +0100

    Python-classes: task 1: adding documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+"""class Square that defines a square """
+
 class Square:
     """ Represent a square, with size."""
+
     def __init__(self, size):
-        """Initializes the data."""
+        """ Initializes a new square.
+        Args:
+            size (int): represents the size of the nez square
+        """
         self.__size = size
