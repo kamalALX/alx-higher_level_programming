ba5e5907d4aa3a1831cf7626293cf870d3c669e2
Kamal <kamalmiftah01@gmail.com>
2024-01-23 19:41:17 +0100

    solving task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square """
+
+
+class Square:
+    """ Represent a square, with size."""
+
+    def __init__(self, size=0):
+        """ Initializes a new square.
+        Args:
+            size (int): represents the size of the nez square
+        """
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an intiger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
