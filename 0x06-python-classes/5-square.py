7184bc7110d8244c9a891cc6f2e6094b5c9e0ec0
Kamal <kamalmiftah01@gmail.com>
2024-01-23 23:41:11 +0100

    fixing task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square """
+
+
+class Square:
+    """ Represent a square, with size."""
+
+    def __init__(self, size=0):
+        """ Initializes a new square.
+        Args:
+            size (int): represents the size of the nez square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ returns the current square area """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.size == 0:
+            print("")
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
