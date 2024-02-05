05bc23f9c2834f831f5ad486eb49f590724ce0e5
Kamal <kamalmiftah01@gmail.com>
2024-02-05 21:08:46 +0100

    fixing task 9, 10, 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,9 +11,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ a method that validates value """
         if type(value) != int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
