138b50b3f174b7ce5de9b98650ca2e048892ddf6
Kamal <kamalmiftah01@gmail.com>
2024-02-06 19:54:26 +0100

    solving task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,6 @@
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_str):
     """ Return: object (Python data structure) represented by a JSON string"""
-    return json.loads(my_obj)
+    return json.loads(my_str)
