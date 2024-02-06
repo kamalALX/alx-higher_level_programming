028ac5a1603bbd97dacd37afedba6ab0875cf2fe
Kamal <kamalmiftah01@gmail.com>
2024-02-06 19:33:25 +0100

    solving task 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """ write 'text' into 'filename' """
+    """ write 'text' into 'filename'
+        Return: the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defiens a function that appends a text to a text file """
+
+
+def write_file(filename="", text=""):
+    """ append 'text' to the end of 'filename'
+        Return: the number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
