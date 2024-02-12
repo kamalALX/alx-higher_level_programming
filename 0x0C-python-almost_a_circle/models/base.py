9aef31f06695a5556ebda2f5843656898ab761cc
Kamal <kamalmiftah01@gmail.com>
2024-02-12 21:50:31 +0100

    Solving task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,13 +44,13 @@ class Base:
         else:
             return json.loads(json_string)
 
-        @classmethod
-        def create(cls, **dictionary):
-            """ returns an instance with all attributes already set """
-            if dictionary and dictionary != {}:
-                if cls.__name__ == "Rectangle":
-                    dummy = cls(1, 1)
-                else:
-                    dummy = cls(1)
-                dummy.update(**dictionary)
-                return dummy
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                dummy = cls(1, 1)
+            else:
+                dummy = cls(1)
+            dummy.update(**dictionary)
+            return dummy
