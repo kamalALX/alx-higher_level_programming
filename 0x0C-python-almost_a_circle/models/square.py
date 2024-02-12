5a972a2531c8f1dcacb3355c00b2eee6a991f6ee
Kamal <kamalmiftah01@gmail.com>
2024-02-12 20:25:52 +0100

    Fixin task 10: addin class documentation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Class Square """
     def __init__(self, size, x=0, y=0, id=None):
         """ initialization method """
         super().__init__(size, size, x, y, id)
