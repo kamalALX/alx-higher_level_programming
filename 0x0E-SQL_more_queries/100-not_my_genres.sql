48dbb46249b4ac2d10900dbb86370e69a7e46ea4
Kamal <kamalmiftah01@gmail.com>
2024-03-13 22:37:44 +0000

    Solving advanced tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,10 @@
 -- This script lists all genres not linked to the show 'Dexter' in the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-SELECT tv_genres.name 
-FROM tv_genres 
-WHERE tv_genres.id NOT IN (
-    SELECT tv_show_genres.genre_id 
-    FROM tv_show_genres 
-    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
-    WHERE tv_shows.title = 'Dexter'
-) 
-ORDER BY tv_genres.name ASC;
+SELECT name FROM tv_genres
+WHERE name NOT IN
+(SELECT name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC;
--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,7 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(rating) AS rating_sum
-FROM hbtn_0d_tvshows_rate
-GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
