a183e3074aa02010cbc3453ed20f69b5331c69c4
kamal <kamalmiftah01@gmail.com>
2024-04-09 16:20:37 +0000

    feat: solving task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+	let occurence = 0;
+  list.map(myFunction);
+  function myFunction (value) {
+    if (value === searchElement) {
+      occurence += 1;
+    }
+  }
+  return occurence;
+};
