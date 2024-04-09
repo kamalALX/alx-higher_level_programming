f612e70f280b32f06fed026213f106c60d81375b
kamal <kamalmiftah01@gmail.com>
2024-04-09 15:49:31 +0000

    feat: solving tasks from 0 to 3

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+class Reactangle {
+}
+
+module.exports = Reactangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+module.exports = class Reactangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
