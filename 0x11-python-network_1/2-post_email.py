c33ac0944170bcb694593a01f57e78e2d1923bd0
Kamal <kamalmiftah01@gmail.com>
2024-04-26 20:54:26 +0100

    solving task 3

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL.
+- sends a request to the URL
+- displays the value of the X-Request-Id variable found in the header of the response.
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    value = {"email": sys.srgv[2]}
+
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
