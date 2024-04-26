93632b8f2fe01ffb4704b497371d0db4a235afd0
Kamal <kamalmiftah01@gmail.com>
2024-04-26 20:58:03 +0100

    solving task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     import urllib.parse
 
     url = sys.argv[1]
-    value = {"email": sys.srgv[2]}
+    value = {"email": sys.argv[2]}
 
     data = urllib.parse.urlencode(value).encode("ascii")
 
