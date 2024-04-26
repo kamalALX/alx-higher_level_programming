c1dd2d0a7f273adb177a34e665f617c097327e9b
Kamal <kamalmiftah01@gmail.com>
2024-04-26 23:18:07 +0100

    solving task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.put(sys.argv[1], params=sys.argv[2])
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(r.text)
