8c7322dfe06ceeae575cec0f8c1180b35ff8d0f0
Kamal <kamalmiftah01@gmail.com>
2024-04-26 18:40:06 +0100

    solving task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
-    print("Body resonse:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
+        print("Body resonse:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
