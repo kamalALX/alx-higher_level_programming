14571a8c4c2f8c4e0cf7a5bad6e2833e9f647701
Kamal <kamalmiftah01@gmail.com>
2024-04-27 01:05:32 +0100

    solving task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a Python script that takes in a letter and
+- sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        letter = sys.argv[1]
+    except:
+        letter = ""
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': letter})
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
