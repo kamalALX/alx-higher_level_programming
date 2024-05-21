63d69854330f7b97f084d76126ad9c2fc75db8bf
Kamal <kamalmiftah01@gmail.com>
2024-05-21 20:17:44 +0100

    task solution

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = 18;
-
-request(apiUrl, { json: true }, (error, response, body) => {
-  if (error) {
-    console.error('Error', error);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-
-  const moviesWithWedge = body.results.filter(movie =>
-    movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-  );
-
-  console.log(`${moviesWithWedge.length}`);
 });
