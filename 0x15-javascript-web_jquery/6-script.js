66e4563fbc12204199db44c172fbb99ed5aebc6b
KamalALX <kamalmiftah01@gmail.com>
2024-05-31 00:06:34 +0100

    task solution

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+/* global $ */
+$(document).ready(function () {
+  $('div#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
+});
--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+/* global $ */
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+    $('DIV#character').text(data.name);
+  });
+});
