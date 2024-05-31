d740e9fcb05058b2dee50820ef5c5748cd3e70ad
KamalALX <kamalmiftah01@gmail.com>
2024-05-31 21:33:14 +0100

    task solution

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/* golabl $ */
+$(document).ready(function() {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json' , function (response) {
+    const movies = response.results;
+    movies.forEach(function(movie) {
+      $('<li></li>').text(movie.title).appendTo('ul#list_movies')
+	  });
+	});
+});
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+/* global $ */
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (response) {
+    $('div#hello').text(response.hello)
+  })
+});
